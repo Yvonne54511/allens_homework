13212d17ddc3bb64a86c2a8e31503926fb023b67
eric <eric55339944@gmail.com>
2026-02-23 18:31:26 +0800

    新增Eric_Product_Web專案

---
+++ 新增Eric_Product_Web專案/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace 新增Eric_Product_Web專案.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
